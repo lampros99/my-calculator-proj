4fca9865f51e65625acee20f7eae8f05851c608f
lampros alexandris <lamprospatmos19@gmail.com>
2024-12-16 19:42:28 +0200

    project created

---
+++ src/gr/aueb/cf/ch2/SubCalculator.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch2;
+
+public class SubCalculator {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 10;
+        int result = 0;
+
+       result = sub(num1, num2);
+
+        System.out.println(result);
+
+    }
+    public static int sub(int a, int b){
+        return  a + b;
+    }
+}
